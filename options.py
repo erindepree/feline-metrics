f4a6411342e3f0a4dd9f6a1bc3a1c0297d3c5555
Erin De Pree <erindepree@gmail.com>
2025-07-21 17:42:10 -0400

    cleaned data

---
+++ options.py
@@ -0,0 +1,26 @@
+# Options
+
+vaccines = [
+    {
+        'id': 'rabies_1y', 
+        'name': 'Rabies, 1 yr', 
+        'disease': 'rabies'
+    },
+    {
+        'id': 'rabies_3y', 
+        'name': 'Rabies, 3 yr', 
+        'disease': 'rabies'
+    },
+    {
+        'id': 'fvrcp_3y', 
+        'name': 'FVRCP, 3 yr', 
+        'disease': 'feline viral rhinotracheitis (FVR), feline calicivirius (C), feline panleukopenia (P)', 
+        'alt_id': 'distemper'
+    }, 
+    {
+        'id': 'fvrcp_1y', 
+        'name': 'FVRCP, 1 yr', 
+        'disease': 'feline viral rhinotracheitis (FVR), feline calicivirius (C), feline panleukopenia (P)', 
+        'alt_id': 'distemper'
+    }
+]
